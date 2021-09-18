a8f8a4aa03f7f5156d3615efe554656ad32b552a
Sawee Rawal <saweerawal1998@gmail.com>
2021-09-18 18:30:17 +0530

    build the dolan-duck-speak app

---
+++ index.js
@@ -0,0 +1,23 @@
+var inputText = document.querySelector("#input-area");
+var btn = document.querySelector("#trans-btn");
+var outputText = document.querySelector("#output");
+
+
+function contructUrl(text) {
+    var server = "https://api.funtranslations.com/translate/dolan.json" + "?text=" + text;
+    return server;
+
+}
+
+function tanslator(event) {
+    var text = inputText.value;
+    var url = contructUrl(text);
+    fetch(url).then(response => response.json()).then(json => {
+        outputText.innerText = json.contents.translated;
+    }).catch(() => alert("error occured"));
+
+
+}
+
+
+btn.addEventListener("click", tanslator)
